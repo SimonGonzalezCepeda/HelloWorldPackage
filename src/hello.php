8e757b6d6ec109be9cab15e4b9d72231e228c122
SimonGonzalezCepeda <simongonzalez@iesebre.com>
2016-10-04 16:24:17 +0200

    Innitial Commit

---
+++ src/hello.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sylver
+ * Date: 4/10/16
+ * Time: 16:04
+ */
+
+namespace Sylver;
+
+
+class hello
+{
+    public function say()
+    {
+        return "Hello world!";
+    }
+}
---
+++ tests/index.php
@@ -0,0 +1,7 @@
+<?php
+
+use Sylver\Hello;
+
+$hello = new Hello();
+
+$hello->say();
